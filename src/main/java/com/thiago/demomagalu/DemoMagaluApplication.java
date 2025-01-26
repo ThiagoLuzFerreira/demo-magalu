9e8b900b6498cac8ff16ec6b8c380cf0a4a47614
ThiagoLuzFerreira <thiagoferreira.psti@poupex.com.br>
2025-01-26 16:49:16 -0300

    commmit inicial

---
+++ src/main/java/com/thiago/demomagalu/DemoMagaluApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.demomagalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/demomagalu/DemoMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.demomagalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
